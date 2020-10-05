851b0b07c8db78139c1562929f3b1dba43689f0c
Spencer Burris <sburris@posteo.net>
2020-10-04 20:23:54 -0700

    Nicer terminal output

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 pub mod messages {
     use std::io::{Read, Write};
     use std::net::{TcpStream};
+    use super::stdout;
 
     /// Handles messages
     pub fn handle_client(mut stream: TcpStream) {
@@ -10,33 +11,35 @@ pub mod messages {
 
         // MPD client connection response
         send(&mut stream, "OK MPD 0.21.11\n");
-        println!("Connection established");
+        stdout::print_mpdrs("Connection established");
 
         // Interpret commands
         let mut cmd_list_mode = false;
         stream.read(&mut data).unwrap();
 
         for line in String::from_utf8_lossy(&data[..]).lines() {
+            stdout::print_client(format!("'{}'", line).as_str());
+
             // Switch to another handler if in a command list block
             if cmd_list_mode {
                 cmd_list_mode = handle_command(line);
-                println!("OK");
                 send(&mut stream, "OK\n")
 
             } else {
                 match line {
                     // Enter or exit command list block
                     "command_list_ok_begin" => {
-                        println!("MESSAGE: '{}', Entering command mode", line);
+                        stdout::print_mpdrs("Entering command mode");
                         cmd_list_mode = true
                     },
                     "command_list_end" => {
-                        println!("MESSAGE: '{}', Exiting command mode from client handler", line);
+                        // this probably shouldn't exist
+                        stdout::print_mpdrs("Exiting command mode from client handler");
                         cmd_list_mode = false;
                     },
 
                     // Everything else
-                    _ => println!("MESSAGE: '{}'", line),
+                    _ => stdout::print_mpdrs("Unhandled message received."),
                 }
             }
         }
@@ -47,12 +50,12 @@ pub mod messages {
         match command {
             // Exit command list mode
             "command_list_end" => {
-                println!("COMMAND: '{}', Exiting command mode", command);
+                stdout::print_mpdrs("Exiting command mode");
                 return false
             },
             // "status" => println!("GOT STATUS COMMAND"),
             _ => {
-                println!("COMMAND: '{}'", command);
+                stdout::print_mpdrs("Unhandled command received.");
                 return true
             }
         }
@@ -60,7 +63,31 @@ pub mod messages {
 
     /// Sends a message
     fn send(stream: &mut TcpStream, message: &str) {
+        stdout::print_mpdrs(format!("'{}'", message).as_str());
         stream.write(message.as_bytes()).unwrap();
         stream.flush().unwrap();
+    }
+}
+
+/// Standard output utilities
+pub mod stdout {
+    use termion::{color, style};
+
+    /// Prints a server-related message in a nice format
+    pub fn print_mpdrs(message: &str) {
+        println!("[{bold}{green}MPDRS{reset}]: {message}",
+            bold = style::Bold,
+            green = color::Fg(color::Magenta),
+            reset = style::Reset,
+            message = message.replace("\n", ""));
+    }
+
+    /// Prints a client-related message in a nice format
+    pub fn print_client(message: &str) {
+        println!("[{bold}{green}CLIENT{reset}]: {message}",
+            bold = style::Bold,
+            green = color::Fg(color::Cyan),
+            reset = style::Reset,
+            message = message.replace("\n", ""));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::TcpListener;
 use std::thread;
 use clap::{App, Arg};
-use mpdrs::messages;
+use mpdrs::{messages, stdout};
 
 fn main() {
-    let args = App::new("MPDrs")
+    let args = App::new("MPDRS")
         .version("0.1.0")
         .about("MPD-compatible music daemon written in Rust")
         .author("Spencer Burris <sburris@posteo.com>")
@@ -23,12 +23,12 @@ fn main() {
     // Bind to port
     let port = args.value_of("port").unwrap_or("6600");
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
-    println!("MPDRS started on port {}", port);
+    stdout::print_mpdrs(format!("Started on port {}", port).as_str());
 
     for stream in listener.incoming() {
         // Spawn worker thread
         thread::spawn(|| {
-            println!("Client connected");
+            stdout::print_mpdrs("Client connected");
             messages::handle_client(stream.unwrap());
         });
     }
